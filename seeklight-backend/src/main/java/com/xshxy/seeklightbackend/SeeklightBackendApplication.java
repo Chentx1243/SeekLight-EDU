7d40d44166ca9760bd7708eb330e77edc1d5085f
chentx1243 <c1243085877@gmail.com>
2025-08-30 17:00:32 +0800

    build(backend): 添加 MyBatis-Plus 和 Langchain4j依赖
    - 在 application.yml 中添加 MyBatis-Plus 全局配置
    - 在 pom.xml 中添加 MyBatis-Plus、Lombok 和 Langchain4j 相关依赖
    - 配置逻辑删除字段

--- seeklight-backend/src/main/java/com/xshxy/seeklightbackend/SeeklightBackendApplication.java
+++ seeklight-backend/src/main/java/com/xshxy/seeklightbackend/SeeklightBackendApplication.java
@@ -1,9 +1,11 @@
 package com.xshxy.seeklightbackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.xshxy.seeklightbackend.mapper")
 public class SeeklightBackendApplication {
 
     public static void main(String[] args) {
